c628dcc18fcc4318f1caf25f808d4fb64b3a138b
Wilson Esmundo <wilson.esmundo@aussiebroadband.com.au>
2020-06-07 14:03:16 +1000

    User Schema & SIgnup # Virtual fields & methods # Errhandler express-val

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
+
 require('dotenv').config();
 //import routes
 const userRoutes = require('./routes/user');
@@ -15,6 +20,12 @@ mongoose
     useUnifiedTopology: true
   })
   .then(() => console.log('MongoDB connected'));
+
+//middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
 
 // routes middleware
 app.use('/api', userRoutes);
