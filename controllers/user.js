1b15a64c1769e122f710853f2895dfa13e8ed686
Wilson Esmundo <wilson.esmundo@aussiebroadband.com.au>
2020-06-07 15:10:10 +1000

    implement express-validator # User signin w/ jwt # user signout route

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
 const { errorHandler } = require('../helpers/dbErrorHandler');
+const jwt = require('jsonwebtoken'); // generate signed token
+const expressJwt = require('express-jwt'); // authorization
 
 exports.signup = (req, res) => {
   //   console.log('req.body', req.body);
@@ -16,4 +18,35 @@ exports.signup = (req, res) => {
       user
     });
   });
+};
+
+exports.signin = (req, res) => {
+  // find user based on email
+  const { email, password } = req.body;
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: 'User with that email does not exist. Please signup'
+      });
+    }
+    // if user found make sure email & password match
+    // create authenticate method in User model
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: 'Email and password dont match'
+      });
+    }
+    //generate a signed token with user id and secret
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    // process the token as " t " in cookie with expiry date
+    res.cookie('t', token, { expire: new Date() + 9999 });
+    // return response with user and token to frontend client
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, email, name, role } });
+  });
+};
+
+exports.signout = (req, res) => {
+  res.clearCookie('t');
+  res.json({ message: 'Signout Successfully' });
 };
