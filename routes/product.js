16ec2d63b4e50f03dbbe20653be38714226e478f
Wilson Esmundo <wilson.esmundo@aussiebroadband.com.au>
2020-06-08 13:31:09 +1000

    Category & Product model, routes & controller, file upload & validation, productById, read & delete products

---
+++ routes/product.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const { create, productById, read, remove } = require('../controllers/product');
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
+const { userById } = require('../controllers/user');
+
+router.get('/product/:productId', read);
+router.delete(
+  '/product/:productId/:userId',
+  requireSignin,
+  isAuth,
+  isAdmin,
+  remove
+);
+router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
+router.param('userId', userById);
+router.param('productId', productById);
+
+module.exports = router;
