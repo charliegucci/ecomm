6fb4bc23c58445ba3f575cc7a111946a1efe517a
Wilson Esmundo <wilson.esmundo@aussiebroadband.com.au>
2020-06-08 18:54:18 +1000

    Create React app#Routing page # Menu/active links#shared components#env

---
+++ ecomm-front/src/user/Signin.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../core/Layout';
+
+const Signin = () => (
+  <Layout title='Sign in' description='Sign in to MERN ecomm App'>
+    ...
+  </Layout>
+);
+
+export default Signin;
---
+++ ecomm-front/src/user/Signup.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../core/Layout';
+
+const Signup = () => (
+  <Layout title='Sign up' description='Sign up to MERN ecomm App'>
+    ...
+  </Layout>
+);
+
+export default Signup;
