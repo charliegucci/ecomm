85760a3d79b9c6b134ff21635d9a2d7893b245e8
Wilson Esmundo <wilson.esmundo@aussiebroadband.com.au>
2020-06-08 15:16:47 +1000

    Update Products # added categoryById, read, update, delete, list category

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read, remove } = require('../controllers/product');
+const {
+  create,
+  productById,
+  read,
+  remove,
+  update
+} = require('../controllers/product');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
@@ -12,6 +18,14 @@ router.delete(
   isAuth,
   isAdmin,
   remove
+);
+
+router.put(
+  '/product/:productId/:userId',
+  requireSignin,
+  isAuth,
+  isAdmin,
+  update
 );
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
 router.param('userId', userById);
